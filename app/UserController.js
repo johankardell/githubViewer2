53bf5d52805ae8750af2a835af958c00327ff40a
Johan Kardell <johan.kardell@primalogic.org>
2016-01-25 15:39:47 +0100

    Added repo functionality

--- app/UserController.js
+++ app/UserController.js
@@ -2,17 +2,22 @@
     'use strict';
 
     var UserController = function ($scope, $routeParams, $log, githubFactory) {
-        var onComplete = function (data) {
+        var onUsersComplete = function (data) {
             $scope.user = data;
+            githubFactory.getRepos($scope.user).then(onReposComplete, onError);
+        };
+
+        var onReposComplete = function (data) {
+            $scope.repos = data;
         };
 
         var onError = function (message) {
-            $scope.message = "Error!"
+            $scope.errorMessage = "Error!";
             $log.log(message);
-        }
+        };
 
-        githubFactory.getUser($routeParams.username).then(onComplete, onError);
-    }
+        githubFactory.getUser($routeParams.username).then(onUsersComplete, onError);
+    };
 
     module.controller("UserController", UserController);
 } (angular.module("githubViewer2")));
--- app/githubFactory.js
+++ app/githubFactory.js
@@ -1,18 +1,25 @@
 (function (module) {
     'use strict';
-    
+
     var githubFactory = function ($http) {
         var getUser = function (username) {
             var githubApiUrl = "https://api.github.com/users/";
-
             return $http.get(githubApiUrl + username)
                 .then(function (response) {
                     return response.data;
                 });
         };
 
+        var getRepos = function (user) {
+            return $http.get(user.repos_url)
+                .then(function(response){
+                   return response.data; 
+                });
+        };
+
         return {
-            getUser: getUser
+            getUser: getUser,
+            getRepos: getRepos
         };
     };
 
